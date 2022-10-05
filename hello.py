cb01a974effbbbadffc301d042282b1726bb051d
jokerfvd <jokerfvd@gmail.com>
2022-10-05 12:04:01 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,8 +5,11 @@ SEILA
 print("Hello World\n")
 
 def ler_arquivo(arquivo):
-    with open (arquivo) as f: 
-        linhas = f.readlines
+    """
+    VS
+    """
+    with open (arquivo) as file_new: 
+        linhas = file_new.readlines
     print(linhas)
 
 ler_arquivo("hello.py")
