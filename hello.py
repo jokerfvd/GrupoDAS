000db4bacc61d8037e807d2783d1ed56f790ac24
jokerfvd <jokerfvd@gmail.com>
2022-10-05 11:56:56 -0300

    bla

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
 """
 SEILA
 """
-print("Hello World")
+
+print("Hello World\n")
+
+def ler_arquivo(arquivo):
+    with open (arquivo) as f: 
+        linhas = f.readlines
+    print(linhas)
+
+ler_arquivo("hello.py")
+
+
